f255b124cc952704592bc50fbe49101408c1f218
Kevin Gibbons <bakkot@gmail.com>
2026-04-01 13:29:34 -0700

    commented-out default linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,22 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   {
     files: ['**/*.ts'],
+    extends: [
+      // ...tseslint.configs.recommendedTypeChecked,
+    ],
     languageOptions: {
-      parser: tseslint.parser,
-    },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      parserOptions: {
+        projectService: true,
+      },
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
+      // 'prefer-const': 'off',
+      // '@typescript-eslint/no-unused-vars': 'off',
+      // '@typescript-eslint/no-unsafe-assignment': 'off',
+      // '@typescript-eslint/no-unsafe-argument': 'off',
+      // '@typescript-eslint/no-unsafe-call': 'off',
+      // '@typescript-eslint/no-unsafe-member-access': 'off',
+      // '@typescript-eslint/no-unsafe-return': 'off',
     },
   },
 );
